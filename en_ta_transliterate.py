6c004a6b058752d4603317f59c829236f5e0330b
beingmechon <bharanishraj@gmail.com>
2024-07-22 23:02:12 +0530

    updated codes to support latest version

--- en_ta_transliterate.py
+++ en_ta_transliterate.py
@@ -8,12 +8,16 @@ Module for transliterating tamil to english by using Google's transliteration AP
 import urllib
 import json
 from io import StringIO
+import urllib.request
+import urllib.parse
 
 
 def transliterate(english_text):
 
     response = urllib.request.urlopen("https://inputtools.google.com/request?%s&itc=ta-t-i0-und&num=13&cp=0&cs=0&ie=utf-8&oe=utf-8" % urllib.parse.urlencode({'text': english_text}))
     output = response.read()
+    output = output.decode('utf-8')
+    
     output = StringIO(output)
     t =  json.load(output)
     if t[0] == 'SUCCESS':
